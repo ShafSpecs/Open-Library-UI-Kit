b4121fcee8c6244fcd084cfac8a4d9a275515b7a
ShafSpecs <abbadshaf05@gmail.com>
2021-07-14 22:58:22 +0100

    Finished the sidebar ui, responsiveness not fully fixed.

--- src/modules/main pages/Homepage.jsx
+++ src/modules/main pages/Homepage.jsx
@@ -2,16 +2,26 @@ import React from 'react'
 
 const Homepage = () => {
     return (
+        //Sidebar
         <div className='sidebar'>
+
+          //A segment in the nav bar
             <div className='section'>
+
+              //The main header/title of each segment
               <div className='header'>Welcome</div>
+
+              //A selection of sub-menus
               <div className='menu'>
                 <div className="item">Introduction</div>
                 <div className="item">Getting started</div>
                 <div className="item">Examples</div>
               </div>
             </div>
+
+            //Horizontal line
             <hr/>
+
             <div className='section'>
               <div className='header'>Page Elements</div>
               <div className='menu'>
@@ -20,7 +30,9 @@ const Homepage = () => {
                 <div className="item">Checkboxes</div>
               </div>
             </div>
+
             <hr />
+
             <div className='section'>
               <div className='header'>Components</div>
               <div className='menu'>
@@ -29,46 +41,8 @@ const Homepage = () => {
                 <div className="item">Dropdowns</div>
               </div>
             </div>
+            
             <hr />
-        </div>
-    )
-}
-
-const Sidebar = () => {
-    return (
-        <div>
-            <div class="ui vertical menu">
-  <div class="item">
-    <div class="header">Welcome</div>
-    <div class="menu">
-      <a class="item">Introduction</a>
-      <a class="item">Getting started</a>
-      <a class="item">Examples</a>
-    </div>
-  </div>
-  <div class="item">
-    <div class="header">Page Elements</div>
-    <div class="menu">
-      <a class="item">Buttons</a>
-      <a class="item">Links</a>
-      <a class="item">Inputs</a>
-    </div>
-  </div>
-  <div class="item">
-    <div class="header">Components</div>
-    <div class="menu">
-      <a class="item">Table</a>
-      <a class="item">Dialogs</a>
-    </div>
-  </div>
-  <div class="item">
-    <div class="header">Support</div>
-    <div class="menu">
-      <a class="item">E-mail Support</a>
-      <a class="item">FAQs</a>
-    </div>
-  </div>
-</div>
         </div>
     )
 }
