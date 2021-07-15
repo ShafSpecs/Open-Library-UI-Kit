4f3b990b96e1a9df8446de599d420bd018da584c
ShafSpecs <abbadshaf05@gmail.com>
2021-07-15 22:58:11 +0100

    Middle of work. To start content

---
+++ src/modules/main pages/MobileSidebar.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+
+const MobileSidebar = () => {
+    return (
+            <div className='mobile-navbar'>
+        <div class="ui menu">
+          <button class="ui icon button">
+            <i class="bars icon"></i>
+          </button>
+          <div class="header item">
+            Open Library UI Kit
+          </div>
+        </div>
+
+
+        <div className="mobile-sidebar">
+          <div className='section'>
+            <div className='header'>Welcome</div>
+            <div className='menu'>
+              <div className="item">Introduction</div>
+              <div className="item">Getting started</div>
+              <div className="item">Examples</div>
+            </div>
+          </div>
+          <hr />
+          <div className='section'>
+            <div className='header'>Page Elements</div>
+            <div className='menu'>
+              <div className="item">Buttons</div>
+              <div className="item">Inputs</div>
+              <div className="item">Checkboxes</div>
+            </div>
+          </div>
+          <hr />
+          <div className='section'>
+            <div className='header'>Components</div>
+            <div className='menu'>
+              <div className="item">Menu</div>
+              <div className="item">Tables</div>
+              <div className="item">Dropdowns</div>
+            </div>
+          </div>
+          <hr />
+        </div>
+
+      </div>
+    )
+}
+
+export default MobileSidebar
--- src/modules/main pages/Sidebar.jsx
+++ src/modules/main pages/Sidebar.jsx
@@ -1,89 +1,23 @@
 import React from 'react'
+import DesktopSidebar from './DesktopSidebar'
+import MobileSidebar from './MobileSidebar'
 
 const Sidebar = () => {
-    const windowScreen = screen.availWidth
+  const windowScreen = screen.availWidth
 
-    if (windowScreen < 768) {
-      return (
-        <div className='mobile-navbar'>
-          <div class="ui menu">
-            <button class="ui icon button">
-              <i class="bars icon"></i>
-            </button>
-            <div class="header item">
-              Open Library UI Kit
-            </div>
-          </div>
-        </div>
-      )
-
-      // return(
-      //     <div className="mobile-sidebar">
-      //       <div className='section'>
-      //         <div className='header'>{windowScreen} Welcome</div>
-      //         <div className='menu'>
-      //           <div className="item">Introduction</div>
-      //           <div className="item">Getting started</div>
-      //           <div className="item">Examples</div>
-      //         </div>
-      //       </div>
-      //       <hr/>
-      //       <div className='section'>
-      //         <div className='header'>Page Elements</div>
-      //         <div className='menu'>
-      //           <div className="item">Buttons</div>
-      //           <div className="item">Inputs</div>
-      //           <div className="item">Checkboxes</div>
-      //         </div>
-      //       </div>
-      //       <hr />
-      //       <div className='section'>
-      //         <div className='header'>Components</div>
-      //         <div className='menu'>
-      //           <div className="item">Menu</div>
-      //           <div className="item">Tables</div>
-      //           <div className="item">Dropdowns</div>
-      //         </div>
-      //       </div>
-      //       <hr />
-      //     </div>
-      // )
-    } else if (windowScreen > 767) {
-    return (
-        <div className='sidebar'>
-            <div className='section'>
-              <div className='header'>{windowScreen} Welcome</div>
-              <div className='menu'>
-                <div className="item">Introduction</div>
-                <div className="item">Getting started</div>
-                <div className="item">Examples</div>
-              </div>
-            </div>
-            <hr/>
-            <div className='section'>
-              <div className='header'>Page Elements</div>
-              <div className='menu'>
-                <div className="item">Buttons</div>
-                <div className="item">Inputs</div>
-                <div className="item">Checkboxes</div>
-              </div>
-            </div>
-            <hr />
-            <div className='section'>
-              <div className='header'>Components</div>
-              <div className='menu'>
-                <div className="item">Menu</div>
-                <div className="item">Tables</div>
-                <div className="item">Dropdowns</div>
-              </div>
-            </div>
-            <hr />
-        </div>
-    )}
+  if (windowScreen < 768) {
+    return(
+    <MobileSidebar />
+    )
+  } else if (windowScreen > 767) {
+    return(
+    <DesktopSidebar />
+    )
+  }
 }
 
 window.addEventListener('resize', () => {
-    location.reload()
+  location.reload()
 })
 
 export default Sidebar
