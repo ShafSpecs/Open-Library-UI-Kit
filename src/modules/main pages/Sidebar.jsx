78d03de0dd3a617d44fd1c6d175a6e68fa96ee66
ShafSpecs <abbadshaf05@gmail.com>
2021-07-15 09:37:39 +0100

    Renamed 'Homepage.jsx' to 'Sidebar.jsx'. Added Responsiveness to the side bar and diffrentiated between Tablets and Desktop

--- src/modules/main pages/Sidebar.jsx
+++ src/modules/main pages/Sidebar.jsx
@@ -1,27 +1,26 @@
 import React from 'react'
 
-const Homepage = () => {
+const Sidebar = () => {
+    const windowScreen = screen.availWidth
+
+    if (windowScreen < 768) {
+      return(
+        <div>
+          {windowScreen}
+        </div>
+      )
+    } else if (windowScreen > 767) {
     return (
-        //Sidebar
         <div className='sidebar'>
-
-          //A segment in the nav bar
             <div className='section'>
-
-              //The main header/title of each segment
-              <div className='header'>Welcome</div>
-
-              //A selection of sub-menus
+              <div className='header'>{windowScreen} Welcome</div>
               <div className='menu'>
                 <div className="item">Introduction</div>
                 <div className="item">Getting started</div>
                 <div className="item">Examples</div>
               </div>
             </div>
-
-            //Horizontal line
             <hr/>
-
             <div className='section'>
               <div className='header'>Page Elements</div>
               <div className='menu'>
@@ -30,9 +29,7 @@ const Homepage = () => {
                 <div className="item">Checkboxes</div>
               </div>
             </div>
-
             <hr />
-
             <div className='section'>
               <div className='header'>Components</div>
               <div className='menu'>
@@ -41,11 +38,13 @@ const Homepage = () => {
                 <div className="item">Dropdowns</div>
               </div>
             </div>
-            
             <hr />
-            
         </div>
-    )
+    )}
 }
 
-export default Homepage
+window.addEventListener('resize', () => {
+  location.reload()
+})
+
+export default Sidebar
