919614afdfb17b4c5bfb2c034ddf0de32f28d5f2
ShafSpecs <abbadshaf05@gmail.com>
2021-07-15 18:27:35 +0100

    Working on mobile responsiveness. Added Navbar button, yet to add logic.

--- src/modules/main pages/Sidebar.jsx
+++ src/modules/main pages/Sidebar.jsx
@@ -4,11 +4,50 @@ const Sidebar = () => {
     const windowScreen = screen.availWidth
 
     if (windowScreen < 768) {
-      return(
-        <div>
-          {windowScreen}
+      return (
+        <div className='mobile-navbar'>
+          <div class="ui menu">
+            <button class="ui icon button">
+              <i class="bars icon"></i>
+            </button>
+            <div class="header item">
+              Open Library UI Kit
+            </div>
+          </div>
         </div>
       )
+
+      // return(
+      //     <div className="mobile-sidebar">
+      //       <div className='section'>
+      //         <div className='header'>{windowScreen} Welcome</div>
+      //         <div className='menu'>
+      //           <div className="item">Introduction</div>
+      //           <div className="item">Getting started</div>
+      //           <div className="item">Examples</div>
+      //         </div>
+      //       </div>
+      //       <hr/>
+      //       <div className='section'>
+      //         <div className='header'>Page Elements</div>
+      //         <div className='menu'>
+      //           <div className="item">Buttons</div>
+      //           <div className="item">Inputs</div>
+      //           <div className="item">Checkboxes</div>
+      //         </div>
+      //       </div>
+      //       <hr />
+      //       <div className='section'>
+      //         <div className='header'>Components</div>
+      //         <div className='menu'>
+      //           <div className="item">Menu</div>
+      //           <div className="item">Tables</div>
+      //           <div className="item">Dropdowns</div>
+      //         </div>
+      //       </div>
+      //       <hr />
+      //     </div>
+      // )
     } else if (windowScreen > 767) {
     return (
         <div className='sidebar'>
@@ -44,7 +83,7 @@ const Sidebar = () => {
 }
 
 window.addEventListener('resize', () => {
-  location.reload()
+    location.reload()
 })
 
 export default Sidebar
