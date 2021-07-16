6ff9fff6c3af055b74aaef6d6d79abeccbbc1257
ShafSpecs <abbadshaf05@gmail.com>
2021-07-16 19:58:05 +0100

    Added some styling

--- src/modules/main pages/PageLayout.jsx
+++ src/modules/main pages/PageLayout.jsx
@@ -10,6 +10,7 @@ const PageLayout = ({title='Page Title', description=`This page's description`, 
                     <h2 className='title'>{title}</h2>
                     <h5 className='desc'>{description}</h5>
                 </div>
+                <div class="ui divider"></div>
                 <div className={classname}>children</div>
             </div>
         </div>
