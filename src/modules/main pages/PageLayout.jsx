3baa6754f8d92fb4e355bf02803590e77f84d9ec
ShafSpecs <abbadshaf05@gmail.com>
2021-07-16 19:25:50 +0100

    Contolling the viewport

--- src/modules/main pages/PageLayout.jsx
+++ src/modules/main pages/PageLayout.jsx
@@ -5,11 +5,13 @@ const PageLayout = ({title='Page Title', description=`This page's description`, 
     return (
         <div className='pagelayout'>
             <Sidebar />
-            <div className="pagetitle">
-                <h2 className='title'>{title}</h2>
-                <h5 className='desc'>{description}</h5>
+            <div className='mainpage'>
+                <div className="pagetitle">
+                    <h2 className='title'>{title}</h2>
+                    <h5 className='desc'>{description}</h5>
+                </div>
+                <div className={classname}>children</div>
             </div>
-            <div className={classname}>children</div>
         </div>
     )
 }
