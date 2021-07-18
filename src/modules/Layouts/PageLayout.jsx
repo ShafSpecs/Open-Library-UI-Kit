35cb4e3e5574c40b7bc4c8eb675f1fb5dd8bd392
ShafSpecs <abbadshaf05@gmail.com>
2021-07-18 18:23:43 +0100

    Deleted Sidebar.jsx, heavy editing in Layouts and style.

--- src/modules/Layouts/PageLayout.jsx
+++ src/modules/Layouts/PageLayout.jsx
@@ -1,58 +1,11 @@
-// import React from 'react'
-// import Sidebar from '../unique imports/Sidebar'
-// import ComponentTab from './ComponentTab'
-
-// const PageLayout = ({title='Page Title', description=`This page's description`, classname }) => {
-//     return (
-//         <div className='pagelayout'>
-//             <Sidebar />
-//             <div className='mainpage'>
-//                 <div className="pagetitle">
-//                     <h1 className='title'>{title}</h1>
-//                     <h5 className='desc'>{description}</h5>
-//                 </div>
-//                 <div class="ui divider"></div>
-//                 <div class="pusher">
-//                     <div className={classname}>
-//                         <ComponentTab />
-//                     </div>
-//                 </div>
-//             </div>
-//         </div>
-//     )
-// }
-
-// export default PageLayout
-
-
 import React from 'react'
 import Sidebar from '../unique imports/Sidebar'
 import ComponentTab from './ComponentTab'
 
-/*<MobileSidebar/> */
-
 const PageLayout = ({title='Page Title', description=`This page's description`, classname }) => {
-    const windowScreen = screen.availWidth
-
-  if (windowScreen < 760) {
-    return(
-    <div className='mainpage'>
-                <div className="pagetitle">
-                    <h1 className='title'>{title}</h1>
-                    <h5 className='desc'>{description}</h5>
-                </div>
-                <div class="ui divider"></div>
-                <div class="pusher">
-                    <div className={classname}>
-                        <ComponentTab />
-                    </div>
-                </div>
-            </div>
-    )
-  } else if (windowScreen > 759) {
-    return(
-    <div className='pagelayout'>
-            <DesktopSidebar />
+    return (
+        <div className='pagelayout'>
+            {screen.availWidth > 759 ? <Sidebar /> : ''}
             <div className='mainpage'>
                 <div className="pagetitle">
                     <h1 className='title'>{title}</h1>
@@ -67,7 +20,6 @@ const PageLayout = ({title='Page Title', description=`This page's description`, 
             </div>
         </div>
     )
-  }
 }
 
 export default PageLayout
---
+++ src/modules/unique imports/MobileNavbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MobileNavbar = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default MobileNavbar
