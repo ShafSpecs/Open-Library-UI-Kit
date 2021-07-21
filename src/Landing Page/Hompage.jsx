393b7c5defb9825a7c129bcf11e32d9ff842ff9d
ShafSpecs <abbadshaf05@gmail.com>
2021-07-21 14:15:59 +0100

    started product section

--- src/Landing Page/Hompage.jsx
+++ src/Landing Page/Hompage.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import MobileNavbar from '../modules/unique imports/MobileNavbar'
 import Header from '../modules/components/Header'
 import image from '../Images/landing-bg.jpg'
+import Product from './Sections/Product'
 
 const Hompage = () => {
     return (
-        <div>
+        <div className='home'>
             {window.innerWidth < 760 ? 
             (<MobileNavbar />) 
             :
@@ -15,6 +16,8 @@ const Hompage = () => {
                     <img src={image} className='bg-image'/>
                 </div>
             </div>
+            <br />
+            <Product />
         </div>
     )
 }
---
+++ src/Landing Page/Sections/Product.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Product = () => {
+    return (
+        <div className='product-home'>
+            <div class="ui stackable four column padded grid">
+                <div class="column" style={{backgroundColor: '#fff', color: '#000',}}>
+                    drtgvcgdcfgh
+                </div>
+                <div class="column" style={{backgroundColor: '#fff', color: '#000',}}>
+                    drtgvcgdcfgh
+                </div>
+                <div class="column" style={{backgroundColor: '#fff', color: '#000',}}>
+                    drtgvcgdcfgh
+                </div>
+                <div class="column" style={{backgroundColor: '#fff', color: '#000',}}>
+                    drtgvcgdcfgh
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Product
