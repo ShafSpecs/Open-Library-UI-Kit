83560da7d963673cdef73eca8ca72f54b6bae1f8
ShafSpecs <69679506+ShafSpecs@users.noreply.github.com>
2021-07-29 20:12:52 +0100

    Create Header.jsx

---
+++ src/modules/components/Header.jsx
@@ -0,0 +1,39 @@
+// import react core components
+import React, { useState, useEffect, useRef } from 'react'
+
+// import Link from React Router
+import { Link } from 'react-router-dom'
+
+const Header = () => {
+    const [navBackground, setNavBackground] = useState(false)
+    const navRef = useRef()
+
+    navRef.current = navBackground
+
+    useEffect(() => {
+        const handleScroll = () => {
+          const show = window.scrollY > 400
+          if (navRef.current !== show) {
+            setNavBackground(show)
+          }
+        }
+        document.addEventListener('scroll', handleScroll)
+
+        return (() => {
+          document.removeEventListener('scroll', handleScroll)
+        })
+    }, [])
+
+    return (
+      <Link to='/'>
+        <div className='landing-page-header'  style={{
+        backgroundColor: `${navBackground ? 'white' : 'transparent'}`, 
+        color: `${navBackground ? 'black' : 'white'}`,
+        }}>
+            OpenLibrary UI Kit
+        </div>
+      </Link>
+    )
+}
+
+export default Header
